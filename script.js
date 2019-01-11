f11d53dcc58ba0059fc8fa1cdb6ff3fe3cac7f75
Rokas <rokas@whatagraph.com>
2019-01-11 10:29:57 +0200

    new taxes

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ let vsd = document.querySelector('.tax-vsd')
 let gpm = document.querySelector('.tax-gpm')
 let totalTax = document.querySelector('.total-tax')
 let afterTax = document.querySelector('.after-tax')
+let totalDays = document.querySelector('.tax-dd')
+let workedDays = document.querySelector('.tax-wd')
+let earnsBeforeTax = document.querySelector('.beforeTax')
 
 for (let i = 0; i < taxInput.length; i++) {
   taxInput[i].addEventListener('keyup', function () {
+    earnsBeforeTax.value = (600 / totalDays.valueAsNumber ) * workedDays.valueAsNumber
     let index = income.valueAsNumber * .7/ 2
     psd.valueAsNumber = 400 * 0.09
     vsd.valueAsNumber = index * 0.309
